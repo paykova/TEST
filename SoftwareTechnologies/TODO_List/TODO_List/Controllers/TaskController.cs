043157ee5d6d3741e248a4f2e3d7dd1f00aeda9c
Snezhana Paykova <paykova@abv.bg>
2018-04-15 01:16:42 +0300

    Update

---
+++ SoftwareTechnologies/TODO_List/TODO_List/Controllers/TaskController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TODOList.Models; 
+
+namespace TODOList.Models
+{
+    public class TaskController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create (Task task)
+        {
+            if (task == null)
+            {
+                return RedirectToAction("Index", "Home"); 
+            }
+
+            using (var db = new TaskDbContext())
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges(); 
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Delete (int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index", "Home"); 
+            }
+
+            using (var db = new TaskDbContext())
+            {
+                var task = db.Tasks.Find(id);
+
+                if (task == null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                db.Tasks.Remove(task);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index", "Home"); 
+        }
+    }
+}
---
+++ SoftwareTechnologies/TODO_List/TODO_List/Models/TaskDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+using TODO_List.Models;
+using TODOList.Controllers;
+
+namespace TODOList.Models
+{
+    public class TaskDbContext : DbContext 
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        //public DbSet<Cat> Cats { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=todo;user=root;");
+        }
+    }
+}
