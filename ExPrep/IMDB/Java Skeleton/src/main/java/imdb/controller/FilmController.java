4568d4d5b340864ac88dbf22079a68e6d04cb783
Snezhana Paykova <paykova@abv.bg>
2018-04-20 10:38:26 +0300

    Update

--- ExPrep/IMDB/Java Skeleton/src/main/java/imdb/controller/FilmController.java
+++ ExPrep/IMDB/Java Skeleton/src/main/java/imdb/controller/FilmController.java
@@ -24,49 +24,92 @@ public class FilmController {
 
 	@GetMapping("/")
 	public String index(Model model) {
+		List<Film> films = filmRepository.findAll();
 
-		// TODO
-		return null;
+		model.addAttribute("films", films);
+		model.addAttribute("view", "film/index");
+
+		return "base-layout";
 	}
 
 	@GetMapping("/create")
 	public String create(Model model) {
-		// TODO
-		return null;
+		model.addAttribute("view", "film/create");
 
+		return "base-layout";
 	}
 
 	@PostMapping("/create")
 	public String createProcess(Model model, FilmBindingModel filmBindingModel) {
 
-		// TODO
-		return null;
+		if(filmBindingModel == null) {
+			return "redirect:/";
+		}
 
-	}
+			if (filmBindingModel.getName().equals(" ") || filmBindingModel.getDirector().equals(" ") || filmBindingModel.getGenre().equals(" ") || filmBindingModel.getYear() == 0) {
+
+				return "redirect:/";
+			}
+
+			Film film = new Film(filmBindingModel.getName(), filmBindingModel.getGenre(), filmBindingModel.getDirector(), filmBindingModel.getYear());
+			filmRepository.saveAndFlush(film);
+
+			return "redirect:/";
+		}
+
 
 	@GetMapping("/edit/{id}")
 	public String edit(Model model, @PathVariable int id) {
-		// TODO
-		return null;
 
+		Film film = filmRepository.findOne(id);
+		if (film == null){
+			return "redirect:/";
+		}
+
+		model.addAttribute("film", film);
+		model.addAttribute("view", "film/edit");
+
+		return "base-layout";
 	}
 
 	@PostMapping("/edit/{id}")
 	public String editProcess(Model model, @PathVariable int id, FilmBindingModel filmBindingModel) {
-		// TODO
-		return null;
+		Film film = filmRepository.findOne(id);
+
+		film.setName(filmBindingModel.getName());
+		film.setDirector(filmBindingModel.getDirector());
+		film.setGenre(filmBindingModel.getGenre());
+		film.setYear(filmBindingModel.getYear());
+
+		filmRepository.saveAndFlush(film);
+		return "redirect:/";
 	}
 
 	@GetMapping("/delete/{id}")
 	public String delete(Model model, @PathVariable int id) {
-		// TODO
-		return null;
+		Film film = filmRepository.findOne(id);
+
+		if (film == null){
+			return "redirect:/";
+		}
+
+		model.addAttribute("film", film);
+		model.addAttribute("view", "film/delete");
+
+		return "base-layout";
 	}
 
 	@PostMapping("/delete/{id}")
 	public String deleteProcess(@PathVariable int id) {
-		// TODO
-		return null;
+		Film film = filmRepository.findOne(id);
 
+		if (film == null){
+			return "redirect:/";
+		}
+
+		filmRepository.delete(id);
+		filmRepository.flush();
+
+		return "redirect:/";
 	}
 }
