f2762361cb633b00af12f2b3f70012b13e22a78d
Snezhana Paykova <paykova@abv.bg>
2018-04-22 13:42:22 +0300

    Update

---
+++ ExPrep/LogNoziroh/CSharp-Skeleton/LogNoziroh/Controllers/ReportController.cs
@@ -0,0 +1,84 @@
+﻿namespace LogNoziroh.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using System.Linq;
+
+    public class ReportController : Controller
+    {
+        private readonly LogNozirohDbContext dbContext;
+
+        public ReportController(LogNozirohDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            var reports = dbContext.Reports.ToList();
+
+            return View(reports);
+        }
+
+        [HttpGet]
+        [Route("details/{id}")]
+        public IActionResult Details(int id)
+        {
+            //var report = dbContext.Reports.Find(id);
+            //return View(report);
+
+            Report report = dbContext
+              .Reports
+              .Where(p => p.Id == id)
+              .FirstOrDefault();
+
+            return View(report);
+
+
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public IActionResult Create()
+        {
+			
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Report report)
+        {
+            dbContext.Reports.Add(report);
+            dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            Report report = dbContext
+                .Reports
+                .Where(p => p.Id == id)
+                .FirstOrDefault();
+
+            return View(report);
+        }
+
+        [HttpPost]
+        [Route("delete/{id}")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirm(Report reportModel)
+        {
+            dbContext.Reports.Remove(reportModel);
+            dbContext.SaveChanges(); 
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ExPrep/LogNoziroh/CSharp-Skeleton/LogNoziroh/Models/Report.cs
@@ -0,0 +1,17 @@
+﻿namespace LogNoziroh.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Report
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Status { get; set; }
+
+        public string Message { get; set; }
+
+        public string Origin { get; set; }
+
+    }
+}
