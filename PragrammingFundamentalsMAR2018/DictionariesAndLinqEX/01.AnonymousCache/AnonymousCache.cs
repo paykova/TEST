a0e6ca55a719693d3d3a58c3a9c833c052cec353
Snezhana Paykova <paykova@abv.bg>
2018-04-23 17:34:08 +0300

    Update

---
+++ PragrammingFundamentalsMAR2018/DictionariesAndLinqEX/01.AnonymousCache/AnonymousCache.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.AnonymousCache
+{
+    class AnonymousCache
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, Dictionary<string, long>> dict = new Dictionary<string, Dictionary<string, long>>(); 
+
+            while (input != "thetinggoesskrra")
+            {
+                string[] infoInput = input.Split(new char[] { ' ', '-', '>', '|'}, StringSplitOptions.RemoveEmptyEntries).ToArray(); 
+
+                if (infoInput.Length == 1)
+                {
+                    string dataSet = infoInput[0];
+                    if (!dict.ContainsKey(dataSet))
+                    {
+                        dict.Add(dataSet, new Dictionary<string, long>()); 
+                    }
+                }
+                if (infoInput.Length == 3)
+                {
+                    string dataKey = infoInput[0];
+                    long dataSize = long.Parse(infoInput[1]);
+                    string dataSet = infoInput[2]; 
+
+                    if (!dict.ContainsKey(dataSet))
+                    {
+                        dict.Add(dataSet, new Dictionary<string, long>()); 
+                    }
+                    if (!dict.ContainsKey(dataKey))
+                    {
+                        dict[dataSet].Add(dataKey, dataSize);
+                    }
+                    dict[dataSet][dataKey] = dataSize; 
+                }
+                input = Console.ReadLine(); 
+            }
+
+            if (dict.Count > 1)
+            {
+                foreach (var item in dict.OrderByDescending(x => x.Value.Values.Sum()))
+                {
+                    long count = item.Value.Values.Sum();
+                    Console.WriteLine($"Data Set: {item.Key}, Total Size: {count}");
+
+                    foreach (var it in item.Value)
+                    {
+                        Console.Write("$." + it.Key + "\n");
+                    }
+                    break;
+                }
+            }            
+        }
+    }
+}
---
+++ PragrammingFundamentalsMAR2018/DictionariesAndLinqEX/02.HornetArmada/HornetArmada.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.HornetArmada
+{
+    class HornetArmada
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, int> shortDict = new Dictionary<string, int>();
+            Dictionary<string, Dictionary<string, int>> longDict = new Dictionary<string, Dictionary<string, int>>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                //{lastActivity} = {legionName} -> {soldierType}:{soldierCount}
+
+                string[] input = Console.ReadLine().Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                int activity =int.Parse(input[0]);
+                string legion = input[1];
+                string type = input[2];
+                int count = int.Parse(input[3]);
+
+
+
+            }
+
+        }
+    }
+}
